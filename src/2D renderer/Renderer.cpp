33fc0e04db17b239b3745b7aa77a893f18d7b5d7
Victor Vimbert-Guerlais <victor.vimbert-guerlais@bginette.fr>
2025-06-08 18:57:02 +0200

    Made the window vertical

--- src/2D renderer/Renderer.cpp
+++ src/2D renderer/Renderer.cpp
@@ -28,50 +28,56 @@ namespace Odin {
 
     void Renderer::Render(const std::array<glm::vec4, 12> &windows_colors) {
 
-    glViewport(0, 0, viewport_width, viewport_height);
+        glViewport(0, 0, viewport_width, viewport_height);
 
-    // First, render to the internal framebuffer
-    fbo_internal.Use();
-    glClearColor(0.f, 0.f, 0.f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // First, render to the internal framebuffer
+        fbo_internal.Use();
+        glClearColor(0.f, 0.f, 0.f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Draw the lights
-    light_shader.use();
-    glBindVertexArray(quadVAO);
+        // Draw the lights
+        light_shader.use();
+        glBindVertexArray(quadVAO);
 
-    for (int i = 0; i < 12; ++i) {
-        glm::vec2 pos; float scale = 0.2f;
-        if (i < 5) pos = { -0.8f + 0.3f * i,  0.5f };
-        else if (i < 7) pos = { -0.2f + 0.3f * (i - 5), 0.0f };
-        else pos = { -0.5f + 0.3f * (i - 7), -0.5f };
+        const float y_top = 0.6f;
+        const float y_mid = 0.0f;
+        const float y_bot = -0.6f;
 
-        light_shader.setVec4("uColor", windows_colors.at(i) * 2.f);
-        light_shader.setVec2("uPosition", glm::vec2(pos[0], pos[1]));
-        light_shader.setFloat("uScale", scale);
+        const float x_spacing = 0.3f;
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        for (int i = 0; i < 12; ++i) {
+            glm::vec2 pos; float scale = 0.24f;
+            if (i < 5) pos = { -0.8f + x_spacing * i,  y_top };
+            else if (i < 7) pos = { -0.2f + x_spacing * (i - 5), y_mid };
+            else pos = { -0.5f + x_spacing * (i - 7), y_bot };
+
+            light_shader.setVec4("uColor", windows_colors.at(i) * 2.f);
+            light_shader.setVec2("uPosition", glm::vec2(pos[0], pos[1]));
+            light_shader.setFloat("uScale", scale);
+
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+        }
+
+        // Prepare the output framebuffer
+        fbo_output.Use();
+        glClearColor(1.f, 0, 0, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Now blit from internal to output
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo_internal.ID());
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo_postProcess.ID());
+        glBlitFramebuffer(0, 0, viewport_width, viewport_height,
+                         0, 0, viewport_width, viewport_height,
+                         GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+        // CRITICAL: Restore the default framebuffer binding for ImGui
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+
+        // Alternatively, you could apply post-processing effects here
+        bloom.Apply(fbo_postProcess, fbo_output, viewport_width, viewport_height);
     }
-
-    // Prepare the output framebuffer
-    fbo_output.Use();
-    glClearColor(1.f, 0, 0, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // Now blit from internal to output
-    glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo_internal.ID());
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo_postProcess.ID());
-    glBlitFramebuffer(0, 0, viewport_width, viewport_height, 
-                     0, 0, viewport_width, viewport_height, 
-                     GL_COLOR_BUFFER_BIT, GL_NEAREST);
-
-    // CRITICAL: Restore the default framebuffer binding for ImGui
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-
-    // Alternatively, you could apply post-processing effects here
-    bloom.Apply(fbo_postProcess, fbo_output, viewport_width, viewport_height);
-}
 
     void Renderer::setViewport(int width, int height) {
         viewport_height = height;
@@ -85,12 +91,12 @@ namespace Odin {
     void Renderer::InitVAO() {
         float quadVertices[] = {
             // pos
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-            -0.5f,  0.5f,
-            -0.5f,  0.5f,
-             0.5f,  0.5f,
-             0.5f, -0.5f,
+            -0.25f, -0.75f,
+             0.25f, -0.75f,
+            -0.25f,  0.75f,
+            -0.25f,  0.75f,
+             0.25f,  0.75f,
+             0.25f, -0.75f,
         };
         glGenVertexArrays(1, &quadVAO);
         glGenBuffers(1, &quadVBO);
